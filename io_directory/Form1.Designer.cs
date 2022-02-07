f33a5e0bcbabe38eb6e2bbbc90b8c7cf13bbe7e9
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 20:41:03 +0530

    Add project files.

---
+++ io_directory/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace io_directory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dir_textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dir_textBox1
+            // 
+            this.dir_textBox1.Location = new System.Drawing.Point(135, 47);
+            this.dir_textBox1.Name = "dir_textBox1";
+            this.dir_textBox1.Size = new System.Drawing.Size(100, 20);
+            this.dir_textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(135, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Check Folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(135, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Move  Folder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 317);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dir_textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dir_textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ io_directory/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace io_directory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             System.IO.Directory.CreateDirectory("E:\\tuts\\" + this.dir_textBox1.Text);
+            Int32 i;
+            for (i = 0; i < 10 ; i++)
+            {
+               System.IO.Directory.CreateDirectory("E:\\tuts\\"+i.ToString() );
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Boolean r;
+            r = System.IO.Directory.Exists("E:\\tuts\\hi");
+            if (r == true)
+            {
+                System.IO.Directory.Delete("E:\\tuts\\hi", true);
+                MessageBox.Show("Done!");
+            }
+            else
+            {
+                MessageBox.Show("Your folder not found!");
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Boolean r;
+            r=System.IO.Directory.Exists("E:\\tuts\\hi");
+            if (r == true)
+            {
+                MessageBox.Show("I found the folder!");
+            }
+            else
+            {
+                MessageBox.Show("Your folder not found!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.IO.Directory.Move("C:\\temp\\koala", "C:\\temp\\test\\bad_koala");
+        }
+    }
+}
